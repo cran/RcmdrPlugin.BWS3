cee7cc1be402857a35b548cb38970f092b093aad
Hideo Aizaki <azk-r@spa.nifty.com>
2023-10-05 06:50:02 +0000

    version 0.1-0

---
+++ R/globals.R
@@ -0,0 +1,6 @@
+if (getRversion() >= '2.15.1') globalVariables(c('buttonsFrame', 
+ 'methodtypeFrame', 'methodtypeVariable', 'subsetFrame', 'subsetVariable', 
+ 'top', 'designmethodVariable', 'designmethodFrame', 'IndependentVarBox', 
+ 'lettertypeFrame', 'lettertypeVariable', 'optoutFrame', 'modeltypeFrame', 
+ 'modeltypeVariable', 'responsetypeFrame', 'responsetypeVariable'))
+
